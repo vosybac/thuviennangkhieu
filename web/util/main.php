43355bf3efadb43a9a307e7166e3184139d20395
vosybac <vosybac@gmail.com>
2022-03-30 15:59:17 +0700

    print app path

--- web/util/main.php
+++ web/util/main.php
@@ -7,6 +7,9 @@ $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING);
 $dirs = explode('/', $uri);
 $app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/';
 
+echo "Application path:";
+echo $app_path;
+
 // Set the include path
 set_include_path($doc_root . $app_path);
 
