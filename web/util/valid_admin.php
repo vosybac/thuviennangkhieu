54140869a032c6c9da6a2b182a26b8f6abbb20c6
vosybac <vosybac@gmail.com>
2022-03-31 14:49:44 +0700

    fixed admin

--- web/util/valid_admin.php
+++ web/util/valid_admin.php
@@ -1,9 +1,6 @@
 <?php
     // make sure the user is logged in as a valid administrator
     if (!isset($_SESSION['admin'])) {
-
-        $admin_site = $doc_root . $app_path. 'admin/account/';
-
-        header('Location: ' . $admin_site);
+        header('Location: ' . $app_path_base . 'admin/account/' );
     }
 ?>
