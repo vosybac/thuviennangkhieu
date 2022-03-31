975c4685542c98ed00f8f8cbf379048032b45ce1
vosybac <vosybac@gmail.com>
2022-03-31 09:55:12 +0700

    refresh

--- web/util/main.php
+++ web/util/main.php
@@ -1,12 +1,11 @@
 <?php
 // Get the document root
-
 $doc_root = filter_input(INPUT_SERVER, 'DOCUMENT_ROOT', FILTER_SANITIZE_STRING);
 
 // Get the application path
 $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING);
 $dirs = explode('/', $uri);
-$app_path = '/' . $dirs[0] . '/' . $dirs[1] . '/';
+$app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/';
 
 // Set the include path
 set_include_path($doc_root . $app_path);
