bf2fdf4a797308d489da3feee9ad7d116eeee9e1
vosybac <vosybac@gmail.com>
2022-03-31 09:46:35 +0700

    add htacess

--- web/model/database.php
+++ web/model/database.php
@@ -1,8 +1,8 @@
 <?php
 // Set up the database connection
-$dsn = 'pgsql:host=ec2-52-3-60-53.compute-1.amazonaws.com;dbname=d7d80jq8ebi2v6';
-$username =  'kuzxoacduwdksq';// //'mgs_user';
-$password =  '20df762c01f44ceb2f7de923ef7b85397ff71d72462af87a1f7a4c8eac21caa8';
+$dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
+$username =  'root';// //'mgs_user';
+$password =  '';//'pa55word';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 try {
