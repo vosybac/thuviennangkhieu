d405ef2602d955063bc81cdab136776ea401fa57
vosybac <vosybac@gmail.com>
2022-04-01 15:11:17 +0700

    Update secure_conn.php

--- web/util/secure_conn.php
+++ web/util/secure_conn.php
@@ -11,6 +11,7 @@
     if (!isset($_SERVER['HTTPS'])) {
         $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
         //header("Location: ".$url);
+        echo "Secure connection";
         exit();
     }
 ?>
