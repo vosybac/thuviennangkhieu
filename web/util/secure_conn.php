d9150b971bfd4950e97b42aee46780c48bf7ac95
vosybac <vosybac@gmail.com>
2022-04-01 15:10:27 +0700

    Update secure_conn.php

--- web/util/secure_conn.php
+++ web/util/secure_conn.php
@@ -10,7 +10,7 @@
     // }
     if (!isset($_SERVER['HTTPS'])) {
         $url = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-        header("Location: ".$url);
+        //header("Location: ".$url);
         exit();
     }
 ?>
