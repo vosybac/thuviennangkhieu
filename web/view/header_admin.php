a8c258eb401ba34d7cece660719acfccdad0710f
vosybac <vosybac@gmail.com>
2022-04-02 19:24:18 +0700

    Update header_admin.php

--- web/view/header_admin.php
+++ web/view/header_admin.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
 
-<!-- the head section -->
 <head>
-    <title>Thư Viện Năng Khiếu</title>
      <link rel="stylesheet" type="text/css"
           href="../../main.css">
 </head>
 
-<body>
-<header>
-    <h1>Thư Viện Năng Khiếu - Admin Page</h1>
-</header>
