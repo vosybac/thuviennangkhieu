1eae6b12c2aba49c33d851d8933b4cebc52a6a02
vosybac <vosybac@gmail.com>
2022-04-16 15:11:19 +0700

    add bootstrap and images to catalog product view

--- web/catalog/category_view.php
+++ web/catalog/category_view.php
@@ -7,6 +7,24 @@
     <?php else: ?>
         <?php foreach ($products as $product) : ?>
         <p>
+
+            <?php
+                // Parse data
+                $category_id = $product['categoryid'];
+                $product_code = $product['productcode'];
+               
+
+                // Get image URL and alternate text
+                $image_filename = $product_code . '_m.png';
+                $image_path = $app_path_base . 'images/' . $image_filename;
+                $image_alt = 'Image filename: ' . $image_filename;
+            ?>
+            <h1><?php echo htmlspecialchars($product_name); ?></h1>
+            <div id="left_column">
+                <p><img src="<?php echo $image_path; ?>"
+                        alt="<?php echo $image_alt; ?>" /></p>
+            </div>
+
             <a href="<?php echo '?product_id=' . $product['productid']; ?>">
                 <?php echo htmlspecialchars($product['productname']); ?>
             </a>
